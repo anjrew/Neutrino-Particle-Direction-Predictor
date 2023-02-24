9b10e38696fd24663dfb0549a707e183abee39c7
Andrew Johnson <andrewmjohnson549@gmail.com>
2023-02-24 17:15:22 +0100

    Fix Tensorflow dependancy issues

--- bin/activate.sh
+++ bin/activate.sh
@@ -1,2 +1,1 @@
 conda activate KAG_IC_NEU
-python -c "import tensorflow as tf; print(tf.config.list_physical_devices('GPU'))"
--- bin/init.sh
+++ bin/init.sh
@@ -1,12 +1,12 @@
-conda create -n KAG_IC_NEU python=3.8
+conda create -n KAG_IC_NEU python=3.9
 conda activate KAG_IC_NEU
 
-conda install -c conda-forge cudatoolkit=11.0.0 cudnn=8.0.0
+# conda install -c conda-forge cudatoolkit=11.0.0 cudnn=8.0.0
 
 pip install -r ./requirements.txt
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/
-mkdir -p $CONDA_PREFIX/etc/conda/activate.d
-echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/' > $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/
+# mkdir -p $CONDA_PREFIX/etc/conda/activate.d
+# echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/' > $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
 
 python -c "import tensorflow as tf; print(tf.config.list_physical_devices('GPU'))"
